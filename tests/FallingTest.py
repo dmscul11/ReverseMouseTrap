330db73c48c7d6dcd5fe86e0b1d74b4ff9ea042d
arunabh-singh <arunabh.singh15@gmail.com>
2018-10-07 00:47:27 -0500

    Basis for 'falling' code

---
+++ tests/FallingTest.py
@@ -0,0 +1,10 @@
+from movements.Falling import *
+
+original, new, binary, color_matrix = load_image(Difficulty.TEST_SMALL_2)
+
+obj_detector = ObjectDetector(new, binary, color_matrix)
+obj_detector.scan_image()
+
+clustered = obj_detector.get_objects()
+
+print(check_stability(obj_detector, 1))
