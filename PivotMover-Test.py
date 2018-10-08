20f26bdc3d41808bd14642f615a6fd353743cb8e
Deirdre Scully <deirdre.m.scully@vanderbilt.edu>
2018-10-08 14:41:37 -0500

    added pivot code, but doesnt exactly pivot

--- PivotMover-Test.py
+++ PivotMover-Test.py
@@ -1,6 +1,7 @@
 from loader.ImageLoader import *
 from detector.ObjectDetector import ObjectDetector
 from movements.StringMover import *
+from movements.Pivoting import *
 import cv2 as cv
 import numpy as np
 
@@ -14,7 +15,13 @@ obj_detector = ObjectDetector(new, binary, color_matrix)
 obj_detector.scan_image()
 objects = obj_detector.get_objects()
 
-# move pivot
+# get pivot lever 14, (15) and pivot 17
+# move pivot random times
+for i in range(10):
+    new_image, new_lever, new_center = rotate_pivot(obj_detector, new, 14, 17, 'counterclockwise')
+    new = np.array(new_image)
+    obj_detector.get_objects()[14].coordinates = list(new_lever)
+    obj_detector.get_objects()[17].coordinates = list(new_center)
 show_image(new)
 
 
